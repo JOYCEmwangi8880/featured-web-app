660f514504b0f05c41786b6e4e4b713e96430597
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-04-12 11:32:53 +0000

    routes

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+posts=[
+    {
+        'author':  'Joyce',
+        'title': 'blog 1 software',
+        'content':'coding',
+        'date_posted':'March 26, 2024'
+    },
+     {
+        'author':  'Mwangi',
+        'title': 'blog 2 Django',
+        'content':'django project',
+        'date_posted':'March 27, 2024'
+    }
+]
+
+
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
