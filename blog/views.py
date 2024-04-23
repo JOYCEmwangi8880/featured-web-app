7d035f5d4ede09a78b6758ee7edd2b3da8924159
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-04-23 10:50:53 +0000

    views.py

--- blog/views.py
+++ blog/views.py
@@ -13,11 +13,17 @@ posts=[
         'content': 'django project',
         'date_posted': 'March 27, 2024'
     },
-         {
+    {
         'author':  'Maggy',
         'title': 'blog 3 Odoo',
         'content': 'odoo project',
         'date_posted': 'April 22, 2024'
+    },
+    {
+        'author':  'Joy',
+        'title': 'blog 4 Odoo',
+        'content': 'odoo project',
+        'date_posted': 'April 23, 2024'
     }
 ]
 
