5cd1ea6c160dc904ef001d2528748c38d907bb90
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-07-31 11:31:36 +0000

    changes

--- blog/views.py
+++ blog/views.py
@@ -19,13 +19,7 @@ posts=[
         'content': 'odoo project',
         'date_posted': 'March 27, 2024'
     },
-    {
-        'author':  'Joy',
-        'title': 'blog 4 odoo',
-        'content': 'odoo project',
-        'date_posted': 'March 27, 2024'
-    },
-   
+    
    
     
 
